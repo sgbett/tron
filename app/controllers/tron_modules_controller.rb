e758669cc686e40b9d2d59ede08d67f8d6b3fd5b
Simon Bettison <simon@bettison.org>
2016-06-30 01:11:02 +0100

    cleanup

--- app/controllers/tron_modules_controller.rb
+++ app/controllers/tron_modules_controller.rb
@@ -3,22 +3,26 @@ class TronModulesController < ApplicationController
   HOST = 'https://beta.portfolioonline.co.uk'
   CLIENT_ID = '33facf6e3c3786c0ad1c45828c57129fa43178064308e1456023a73a675308d1'
   CLIENT_SECRET = '829d0e0e519404b80316a4b290c40bc2af188d38ecfc02f1e63dbdf6463cc4ec'
-  
+
+#   HOST = 'http://portfoliobuilder.dev'
+#   CLIENT_ID = 'd6e5a2eceed01cade48f44d7f77c6d752526ab03de00a67e23a404dd07ca7b87'
+#   CLIENT_SECRET = 'c5a02f6634a4a83f48abd81649462da2070170496b682d0fddcd2033436a0335'
+    
   def show
     #callback url
       if code = params[:code]
-        response = RestClient.post "#{HOST}/oauth/token", {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, code: code , grant_type: 'authorization_code', redirect_uri: request.url.split('?')[0]}
 
-        hash = JSON.parse(response)
+        resource = RestClient::Resource.new("#{HOST}/oauth/token",verify_ssl: false)
+        response = resource.post(client_id: CLIENT_ID, client_secret: CLIENT_SECRET, code: code , grant_type: 'authorization_code', redirect_uri: request.url.split('?')[0])
+
+        access_token = JSON.parse(response)['access_token']
         
-#         render text: RestClient.get("#{HOST}/api/user", {:Authorization => "Bearer #{hash['access_token']}"})
-
         data = {"form"=>{"tool"=>"tron_module","responses"=>{"module_name"=>"Pharmacodynamics=> Part 1","module_url"=>"http=>//tron.rcpsych.ac.uk/default.aspx?page=22559","reflection"=>session[:notes]},"evidence_attributes"=>{"start_date"=>Date.today}}}
 
-        response = RestClient.post "#{HOST}/api/forms", data.to_json, content_type: 'application/json', accept: 'application/json', Authorization: "Bearer #{hash['access_token']}"
+        resource = RestClient::Resource.new("#{HOST}/api/forms.js",verify_ssl: false, headers: { Authorization: "Bearer #{access_token}"})
+        response = resource.post data.to_json, content_type: :json, accept: :json
 
-        render text: response
-
+        redirect_to JSON.parse(response)['activity_url']
 
       else
         render text: params.inspect
